696fa179ef62edcfec44d70a46749a5d077f973d
Damien Vesper <ldamienvesper@gmail.com>
2023-11-26 18:19:00 -0600

    fix: clean dist on dev

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import { defineConfig } from 'vite';
 import prodConfig from './vite/vite.prod';
 import devConfig from './vite/vite.dev';
 
-export default defineConfig(({ command }) => {
-    if (command === `serve`) return devConfig;
-    else return prodConfig;
+import { existsSync, rmSync } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const DIRNAME = dirname(fileURLToPath(import.meta.url));
+export default defineConfig(({ command, mode }) => {
+    if (command === `serve` && mode === `development`) {
+        if (existsSync(resolve(DIRNAME, `./dist`))) rmSync(resolve(DIRNAME, `./dist`), { recursive: true, force: true });
+    }
+
+    return command === `serve` ? devConfig : prodConfig;
 });
